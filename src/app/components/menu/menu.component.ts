0958bc1691f3f51aa74c27e0268acb930721e2c6
Zac <zukaru@aol.com>
2020-11-11 07:33:26 -0600

    Added menu and nav functionality

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public af: AngularFireAuth,
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  signOut() {
+    this.af.signOut()
+      .then(
+        () => {
+          this.route.navigateByUrl('');
+          alert("You have logged out.");
+            
+        }
+      )
   }
 
 }
